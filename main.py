1336500b7b02fc96faa88d4d7bf25c826c100d05
Benedikt <benediktbraun@gmx.de>
2024-04-07 00:52:28 +0200

    Added random forest model
    - Added a random forest model with hyperparameter tuning
    - Added evaluation of that random forest model to main.py
    - Deleted unused funktion from model_training.py
    - Improved code style using only single quotes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_cleaning import clean_data
 from feature_engineering import feature_engineering
-from model_training import evaluate_model, grid_search_logistic_regression
+from model_training import evaluate_model, grid_search_logistic_regression, grid_search_random_forest
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
@@ -16,5 +16,7 @@ train_data = feature_engineering(train_data)
 
 # Training and evaluating
 X_train, X_val, y_train, y_val = train_test_split(train_data.drop('survived', axis=1), train_data['survived'], test_size=0.2)
-model = grid_search_logistic_regression(X_train, y_train)
-evaluate_model(model, X_val, y_val)
+logistic_regression_model = grid_search_logistic_regression(X_train, y_train)
+random_forest_model = grid_search_random_forest(X_train, y_train)
+evaluate_model(logistic_regression_model, X_val, y_val)
+evaluate_model(random_forest_model, X_val, y_val)
--- model_training.py
+++ model_training.py
@@ -2,19 +2,28 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report, precision_recall_curve, roc_curve, auc
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 
 def grid_search_logistic_regression(X_train, y_train):
     param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 30, 50, 80, 100]}
     grid_search = GridSearchCV(LogisticRegression(max_iter=1000), param_grid, cv=5)
     grid_search.fit(X_train, y_train)
-    print("Best parameters:", grid_search.best_params_)
-    print("Best cross-validation score:", grid_search.best_score_)
+    print('Best parameters:', grid_search.best_params_)
+    print('Best cross-validation score:', grid_search.best_score_)
     return grid_search.best_estimator_
 
-def train_model(X_train, y_train):
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
-    return model
+def grid_search_random_forest(X_train, y_train):
+    param_grid = {
+        'n_estimators': [50, 100, 200],
+        'max_depth': [None, 10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+    grid_search = GridSearchCV(RandomForestClassifier(random_state=42), param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+    print('Best parameters:', grid_search.best_params_)
+    print('Best cross-validation score:', grid_search.best_score_)
+    return grid_search.best_estimator_
 
 def evaluate_model(model, X_val, y_val):
     y_predicted = model.predict(X_val)
